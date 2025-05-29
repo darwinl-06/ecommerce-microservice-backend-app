e2596d6de05aadaf946133fdf99367f71b40fe64
darwinl-06 <darwinlenis.06@gmail.com>
2025-05-29 12:38:18 -0500

    fix mysql schemas

--- user-service/src/main/resources/db/migration/V1__create_users_table.sql
+++ user-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -6,8 +6,8 @@ CREATE TABLE users (
 	image_url VARCHAR(255) DEFAULT 'https://bootdey.com/img/Content/avatar/avatar7.png',
 	email VARCHAR(255) DEFAULT 'springxyzabcboot@gmail.com',
 	phone VARCHAR(255) DEFAULT '+21622125144',
-	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
--- user-service/src/main/resources/db/migration/V5__create_credentials_table.sql
+++ user-service/src/main/resources/db/migration/V5__create_credentials_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE credentials (
 	is_account_non_expired BOOLEAN DEFAULT true,
 	is_account_non_locked BOOLEAN DEFAULT true,
 	is_credentials_non_expired BOOLEAN DEFAULT true,
-	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
